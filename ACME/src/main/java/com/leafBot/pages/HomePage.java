d0c7f90e63f29666565c3eaede57b3d1375fad99
kaviy <kaviy@LAPTOP-M6T5B0IU>
2020-02-09 09:32:21 +0530

    First Commit

---
+++ ACME/src/main/java/com/leafBot/pages/HomePage.java
@@ -0,0 +1,41 @@
+package com.leafBot.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.leafBot.testng.api.base.ProjectSpecificMethods;
+
+import cucumber.api.java.en.Then;
+
+public class HomePage extends ProjectSpecificMethods{
+
+	public HomePage(RemoteWebDriver driver, ExtentTest node, ExtentTest test){
+		this.driver = driver;
+		this.node = node;
+		this.test = test;
+		PageFactory.initElements(driver, this);
+	}		
+
+	public void gettitle() {
+		System.out.println(driver.getTitle());
+		
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
